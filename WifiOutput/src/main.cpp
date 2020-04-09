9d58a042e046aafbeb993eca9fb13420334d1c5d
bapds <bapds.1986@gmail.com>
2020-04-09 10:20:09 -0300

    Start

--- WifiOutput/src/main.cpp
+++ WifiOutput/src/main.cpp
@@ -1,132 +1,157 @@
+/*
+ * Program: Controle de uma saída via rede
+ * Author: BAPDS
+ * Created: 30/03/2018
+ * Informações obtidas:
+https://www.usinainfo.com.br/blog/esp32-wifi-comunicacao-com-a-internet/
+acessado em 07/04/2020 as 18:07
+*/
 #include <Arduino.h>
-int btn1_pin = 9;
-int btn2_pin = 10;
-int btn1 = 0;
-int btn2 = 0;
+#include <ESP8266WiFi.h>
 
-int led1 = 0;
-int led2 = 1;
-int led3 = 2;
-int led4 = 3;
-int led5 = 4;
-int led6 = 5;
-int led7 = 12;
-int led8 = 13;
-int led9 = 14;
-int led10 = 15;
-int led11 = 16;
-//int led12 = 33;
+int rele = 0;
+int led1 = 1;
+int led2 = 2;
+int led3 = 3;
+int led4 = 4;
 
 int tempo = 1000;
+const char* ssid = "CasaBr";
+const char* password = "5d8d992e43";
 
-// the setup routine runs once when you press reset:
-void setup() {               
-  pinMode(btn1_pin, INPUT);  
-  pinMode(btn2_pin, INPUT);  
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = -3600*3;
+
+WiFiServer server(80);
+
+void piscar(int pino, int vezes) {
+
+  for(int n = 0; n <= vezes; n++){
+    digitalWrite(pino, HIGH);
+    delay(tempo);
+    digitalWrite(pino, LOW);
+    delay(tempo);
+  }
+}
+
+void ligar(int pino) {
+    digitalWrite(pino, HIGH);
+    delay(tempo);
+}
+
+void desligar(int pino) {
+    digitalWrite(pino, LOW);
+    delay(tempo);
+}
+/*
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+*/
+
+void setup() {  
   
+  Serial.begin(9600); 
+
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-  pinMode(led7, OUTPUT);
-  pinMode(led8, OUTPUT);
-  pinMode(led9, OUTPUT);
-  pinMode(led10, OUTPUT);
-  pinMode(led11, OUTPUT);
+  pinMode(rele, OUTPUT);
 
-}
+  Serial.println();
+  Serial.print("Conectando-se a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
 
-void piscar(int i) {
-    digitalWrite(i, HIGH);
-    delay(tempo);
-    digitalWrite(i, LOW);
-    delay(tempo);
-}
-
-void ligar(int i) {
-    digitalWrite(i, HIGH);
-    delay(tempo);
-}
-
-void desligar(int i) {
-    digitalWrite(i, LOW);
-    delay(tempo);
-}
-
-void func1(bool cmd){
-  if (cmd == true){
-    for (int i=0; i <= 5; i++){
-      ligar(i);
-    }
-    for (int i=12; i <= 16; i++){
-      ligar(i);
-    }
-
-    for (int j=16; j >= 12; j--){
-      desligar(j);
-    } 
-    for (int j=5; j >= 0; j--){
-      desligar(j);
-    } 
+  while (WiFi.status() != WL_CONNECTED) {
+    piscar(led4,1);
+    delay(500);
+    Serial.print("-");
   }
+  // configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  // printLocalTime();
+  // piscar(led3, 2);
+  
+  Serial.println("");
+  Serial.println("WiFi conectada.");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+ 
+  server.begin();
 }
 
-void func2(bool cmd){
-  if (cmd == true){
-
-    for (int j=16; j >= 12; j--){
-      desligar(j);
-    } 
-    for (int j=5; j >= 0; j--){
-      desligar(j);
-    } 
-    cmd = false;
-  }
-}
-
-void func3(bool cmd){
-  if(cmd==true){
-
-    for (int k=0; k <= 5; k++){
-      piscar(k);
-    } 
-    for (int k=12; k <= 16; k++){
-      piscar(k);
-    } 
-  }
-}
 // the loop routine runs over and over again forever:
 void loop() {  
+  WiFiClient client = server.available();
+  
+  ligar(led3);
+  
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
 
-  btn1 = digitalRead(btn1_pin);   // read the input pin
-  btn2 = digitalRead(btn2_pin);   // read the input pin
+    while (client.connected()) {
+      // desligar(led2);
+      // ligar(led1);
 
-  if (true){
-    func1(true);
-    func2(false);
-    func3(false);
-  }
-  else{
-    func1(false);
-  }
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.println("WiFi conectada.");
+            client.println();
+            client.print("Endereco de IP: ");
+            client.println();
+            client.print(WiFi.localIP());
+            client.println();
+            client.print(WiFi.macAddress());
+            client.println();
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        
+        if (currentLine.endsWith("GET /H")) {
+          // digitalWrite(led1, HIGH);
+          // ligar(rele);
+          ligar(led2);
+          piscar(led1, 2);
+        }
+        if (currentLine.endsWith("GET /L")) {
+          //digitalWrite(led1, LOW);
+          // desligar(rele);
+          desligar(led2);
+          piscar(led4, 2);
+        }
+        
+      }
+    }
 
-  if (true){  
-    func2(true);
-    func1(false);
-    func3(false);
+    client.stop();
+    Serial.println("Client Disconnected.");
+    // piscar(led3, 3);
   }
-  else{
-    func2(false);    
-  }
+  
 
-  if (true){
-    func3(true);
-    func1(false);
-    func2(false);
-   } 
-  else{
-    func3(false);
-  }
+  // btn1 = digitalRead(btn1_pin);   // read the input pin
+  // btn2 = digitalRead(btn2_pin);   // read the input pin
+  
 }
